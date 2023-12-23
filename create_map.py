74640cf85dd3670189f9203e8fc7a05f345c0f28
Egor Ivanov <egor.i.ivanov@gmail.com>
2023-12-23 17:41:17 +0100

    extra br

--- create_map.py
+++ create_map.py
@@ -69,7 +69,7 @@ def convert_urls_to_links(text):
     # Для каждого URL заменяем его на текстовое поле с этим URL
     for url in urls:
         #replacement = f'<div style="width:330px;"><input type="text" value="{url}" onclick="this.select();" style="width:100%; border: none;" readonly></div>'
-        replacement = f'<br><div style="width:330px;"><a href="{url}" target="_blank">Link</a></div>'
+        replacement = f'<div style="width:330px;"><a href="{url}" target="_blank" /></div>'
         text = text.replace(url, replacement)
 
     return text
